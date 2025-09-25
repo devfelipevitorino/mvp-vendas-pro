18447f2fe294b3bd71aa471cdd8ef20fa4fdd300
devfelipevitorino <devfelipevitorino@gmail.com>
2025-09-25 20:05:21 -0300

    Altera web.php com novas rotas

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'welcome']);
+
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/create', [ProductController::class, 'create']);
