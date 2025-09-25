ea5277e5f3c20eb0b76123c05ef957f9ea2de139
devfelipevitorino <devfelipevitorino@gmail.com>
2025-09-25 20:04:43 -0300

    Adiciona ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -18,5 +20,43 @@ class ProductController extends Controller
             $products = Product::all();
         }
         return view('welcome', ['products' => $products, 'search' => $search]);
+    }
+
+    public function create()
+    {
+        $lastId = Product::max('id');       
+        $nextId = $lastId ? $lastId + 1 : 1; 
+
+        return view('products.create', ['nextId' => $nextId]);
+    }
+
+
+    public function store(Request $request)
+    {
+
+        $product = new Product();
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('image/products'), $imageName);
+
+            $product->image = $imageName;
+        }
+
+        //$user = Auth::user();
+        //$product->user_id = $user->id;
+
+        $product->save();
+
+        return redirect('/');
     }
 }
