212a15dae7163f3e3a79fb85fab0764dbd0ad939
devfelipevitorino <devfelipevitorino@gmail.com>
2025-09-25 20:23:00 -0300

    Adiciona migration para novas colunas em products

---
+++ database/migrations/2025_09_25_230939_add_columns_to_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('reference');
+            $table->bigInteger('barcode');
+            $table->integer('stock_quantity');
+            $table->string('observation');
+            $table->string('add_info');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('reference');
+            $table->dropColumn('barcode');
+            $table->dropColumn('stock_quantity');
+            $table->dropColumn('observation');
+            $table->dropColumn('add_info');
+        });
+    }
+};
