578575ffebf556d07973adec4ed4213eaddd9e84
devfelipevitorino <devfelipevitorino@gmail.com>
2025-09-25 21:57:11 -0300

    Adiciona novas colunas à tabela products

--- database/migrations/2025_09_25_230939_add_columns_to_products_table.php
+++ database/migrations/2025_09_25_230939_add_columns_to_products_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->string('reference');
             $table->bigInteger('barcode');
             $table->integer('stock_quantity');
-            $table->string('observation');
-            $table->string('add_info');
+            $table->string('observation')->nullable();
+            $table->string('add_info')->nullable();
         });
     }
 
