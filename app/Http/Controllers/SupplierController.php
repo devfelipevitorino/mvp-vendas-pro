5fd174d986ede2081d014c07b29c6a9be337fe95
devfelipevitorino <devfelipevitorino@gmail.com>
2025-09-26 23:53:46 -0300

    Adicionar model Supplier e SupplierController

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    public function create()
+    {
+        $user = auth()->user();
+        $lastSupplier = $user->suppliers()->orderBy('id', 'desc')->first();
+        $nextId = $lastSupplier ? $lastSupplier->id + 1 : 1;
+
+        return view('supplier.create', ['nextId' => $nextId]);
+    }
+
+    public function store(Request $request)
+    {
+        $supplier = new Supplier();
+        $user = auth()->user();
+
+        $supplier->name = $request->name;
+        $supplier->user_id = $user->id;
+        $supplier->save();
+
+        return redirect('/products/create');
+    }
+}
