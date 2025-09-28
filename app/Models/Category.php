ce11f13951c5d9e0b0e3f11bd590fbdd7fe03b57
devfelipevitorino <devfelipevitorino@gmail.com>
2025-09-28 14:40:09 -0300

    fix: ajusta relacionamento products no model Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,4 +13,9 @@ class Category extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id');
+    }
 }
