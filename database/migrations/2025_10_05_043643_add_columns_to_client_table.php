e801c543d62985c70cc47f878b18f8c05dd9dc45
devfelipevitorino <devfelipevitorino@gmail.com>
2025-10-05 01:43:51 -0300

    Adiciona migration para novas colunas na tabela clients

---
+++ database/migrations/2025_10_05_043643_add_columns_to_client_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->string('document');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->integer('number_address');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('uf');
+            $table->string('cep');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->dropColumn('document');
+            $table->dropColumn('email');
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
+            $table->dropColumn('number_address');
+            $table->dropColumn('neighborhood');
+            $table->dropColumn('city');
+            $table->dropColumn('uf');
+            $table->dropColumn('cep');
+        });
+    }
+};
