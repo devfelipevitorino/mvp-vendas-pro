b216e06ac6a6d02c40a71df681a94782d3a3d12c
devfelipevitorino <devfelipevitorino@gmail.com>
2025-10-18 19:09:07 -0300

    Atualiza lógica de controle de produtos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,7 @@ class ProductController extends Controller
 {
     public function welcome()
     {
+        $user = auth()->user();
         $search = request('search');
         if ($search) {
             $products = Product::where([
@@ -18,7 +19,10 @@ class ProductController extends Controller
         } else {
             $products = Product::all();
         }
-        return view('welcome', ['products' => $products, 'search' => $search]);
+
+
+
+        return view('welcome', ['products' => $products, 'search' => $search, 'user' => $user]);
     }
 
     public function create()
@@ -74,9 +78,60 @@ class ProductController extends Controller
     public function list()
     {
         $user = auth()->user();
-        $products = Product::where('user_id', $user->id)
-            ->get();
+        $products = Product::where('user_id', $user->id)->get();
 
         return view('products.list', compact('products'));
+    }
+
+    public function edit($id)
+    {
+        $user = auth()->user();
+
+        $product = Product::findOrFail($id);
+        $categories = $user->categories()->orderBy('name')->get();
+        $suppliers = $user->suppliers()->orderBy('name')->get();
+
+        if ($user->id != $product->user_id) {
+            return redirect('/dashboard');
+        }
+
+        return view('products.edit', ['product' => $product, 'categories' => $categories, 'suppliers' => $suppliers]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->reference = $request->reference;
+        $product->barcode = $request->barcode;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stock_quantity = $request->stock;
+        $product->add_info = $request->add_info;
+        $product->observation = $request->observation;
+        $product->description = $request->description;
+        $product->category_id = $request->category_id;
+
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('image/products'), $imageName);
+
+            $data['image'] = $imageName;
+        }
+
+
+        $product->save();
+
+        if ($request->has('supplier_id')) {
+            $product->suppliers()->sync($request->supplier_id);
+        }
+
+        return redirect('/products/list');
     }
 }
